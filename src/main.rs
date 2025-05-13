a6ffd174e9b16d617f26ab9516e36d472f1ec483
Aba22y <ahmedabbasy2001@gmail.com>
2025-05-13 13:33:18 +1000

    game complete

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Feeling Lucky?");
-    println!("What do you think the random number is?");
     
     let secret_num = rand::thread_rng().gen_range(1..=100);
 
-    //empty instance
-    let mut guess = String::new();
+    loop {
+        println!("What do you think the random number is?");
+        //empty instance
+        let mut guess = String::new();
+        // & is a refrence to the actual variable so we avoid copying it
+        // anything that returns an enumerated result "Ok"/"Err" can use expect
+        io::stdin().read_line(&mut guess).expect("Failed to read");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    // & is a refrence to the actual variable so we avoid copying it
-    // anything that returns an enumerated result "Ok"/"Err" can use expect
-    io::stdin().read_line(&mut guess).expect("Failed to read");
-    
-    println!("You guessed {}", guess);
-    println!("{guess} is quite the number")
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("Winner!");
+                break;
+            }
+        }
+    }
+
 }
